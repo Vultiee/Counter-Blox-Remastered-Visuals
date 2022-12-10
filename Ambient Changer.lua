c7990910eef9a93c1ec86af65d8c2769369b2f09
Vultiee <80932924+Vultiee@users.noreply.github.com>
2022-12-10 01:37:42 -0500

    Update Ambient Changer.lua

--- Ambient Changer.lua
+++ Ambient Changer.lua
@@ -1,3 +1,4 @@
 game:GetService("Lighting").Ambient = Color3.fromRGB(141, 115, 245)
 game:GetService("Lighting").ColorShift_Top = Color3.fromRGB(141, 115, 245)
 game:GetService("Lighting").ColorShift_Bottom = Color3.fromRGB(141, 115, 245)
+game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(141, 115, 245)  
